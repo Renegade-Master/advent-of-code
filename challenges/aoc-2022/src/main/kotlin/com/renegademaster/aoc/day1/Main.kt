867d072f1191fa8b6b98c09ad079e8aa529742fe
Renegade-Master <renegade.master.dev@protonmail.com>
2022-12-01 21:30:40 +0000

    Solution to part 2;

--- challenges/aoc-2022/src/main/kotlin/com/renegademaster/aoc/day1/Main.kt
+++ challenges/aoc-2022/src/main/kotlin/com/renegademaster/aoc/day1/Main.kt
@@ -6,6 +6,63 @@ import kotlin.system.exitProcess
 
 private val logger = KotlinLogging.logger {}
 
+fun getHighestTripleGroupTotal(calorieList: ArrayList<ArrayList<Int>>): List<Int> {
+    val topThreeGroupTotals: ArrayList<Int> = ArrayList()
+    var groupTotal: Int
+
+    logger.info { "Computing highest group total..." }
+
+    calorieList.forEach { group ->
+        groupTotal = 0
+
+        // Add up the current group
+        group.forEach {
+            logger.debug("Adding [$it] to [$groupTotal]")
+            groupTotal += it
+        }
+
+        if (topThreeGroupTotals.size < 3) {
+            topThreeGroupTotals.add(groupTotal)
+            return@forEach
+        }
+
+        topThreeGroupTotals.sort()
+
+        // Check if this group has a higher total
+        if (groupTotal > topThreeGroupTotals[0]) {
+            logger.debug("[$groupTotal] is higher than [${topThreeGroupTotals[0]}].")
+
+            if (groupTotal > topThreeGroupTotals[1]) {
+                logger.debug("[$groupTotal] is higher than [${topThreeGroupTotals[1]}].")
+
+                if (groupTotal > topThreeGroupTotals[2]) {
+                    logger.debug("[$groupTotal] is higher than [${topThreeGroupTotals[2]}]. Setting new highest value.")
+                    topThreeGroupTotals[0] = topThreeGroupTotals[1]
+                    topThreeGroupTotals[1] = topThreeGroupTotals[2]
+                    topThreeGroupTotals[2] = groupTotal
+                    return@forEach
+                }
+
+                logger.debug("Setting new highest value.")
+                topThreeGroupTotals[0] = topThreeGroupTotals[1]
+                topThreeGroupTotals[1] = groupTotal
+                return@forEach
+            }
+
+            logger.debug("Setting new highest value.")
+            topThreeGroupTotals[0] = groupTotal
+            return@forEach
+        } else {
+            logger.debug("[$groupTotal] is not higher than [${topThreeGroupTotals}]")
+        }
+    }
+
+    logger.info { "Determined highest group total." }
+
+    return topThreeGroupTotals
+}
+
+
 fun getHighestGroupTotal(calorieList: ArrayList<ArrayList<Int>>): Int {
     var highestTotalCalories: Int = -1
     var groupTotal: Int
@@ -15,11 +72,13 @@ fun getHighestGroupTotal(calorieList: ArrayList<ArrayList<Int>>): Int {
     calorieList.forEach { group ->
         groupTotal = 0
 
+        // Add up the current group
         group.forEach {
             logger.debug("Adding [$it] to [$groupTotal]")
             groupTotal += it
         }
 
+        // Check if this group has a higher total
         if (groupTotal > highestTotalCalories) {
             logger.debug("[$groupTotal] is higher than [$highestTotalCalories]. Setting new highest value.")
             highestTotalCalories = groupTotal
@@ -87,6 +146,9 @@ fun main(args: Array<String>) {
     val calorieList = getCalorieList(data)
 
     val highestGroupTotal = getHighestGroupTotal(calorieList)
-
     println("Total calories of the group with the highest combined calories: [$highestGroupTotal]")
+
+    val highestTripleGroupTotal = getHighestTripleGroupTotal(calorieList)
+    println("Top 3 groups with the highest combined calories: [${highestTripleGroupTotal}]")
+    println("Total calories of the top 3 groups with the highest combined calories: [${highestTripleGroupTotal.sum()}]")
 }
